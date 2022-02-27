0f1b04c7c7fe51dfc701c93ae5dcb87da900a5dd
Adi <blue.vixen@gmail.com>
2022-02-27 23:33:22 +0200

    make it better

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const http = require('http').createServer(app)
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static(path.resolve(__dirname, 'public')))
-    app.use(express.static('public'));
+    app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
     const corsOptions = {
         origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
