f12170a0393c659d127770483c0e6dfe968975e7
Adi <blue.vixen@gmail.com>
2022-02-27 17:44:16 +0200

    pushing to heroku

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.use('/api', pingRoutes)
 app.use('/api/tophosts', pingRoutes)
 
 
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 const port = process.env.PORT || 3030
 http.listen(port, () => {
